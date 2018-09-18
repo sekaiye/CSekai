d9a285ecf9677162766b97cd5b0bf600568e0c42
sekaiye <8029936@qq.com>
2018-09-18 17:24:09 +0800

    email

--- CSekai.SYS.Entity/User.cs
+++ CSekai.SYS.Entity/User.cs
@@ -9,5 +9,7 @@ namespace CSekai.SYS.Entity
         public string NickName{get;set;}
         public string Pwd{get;set;}
         public int Forbid{get;set;}
+        public string Email { get; set; }
+        public string Cellphone { get; set; }
     }
 }
